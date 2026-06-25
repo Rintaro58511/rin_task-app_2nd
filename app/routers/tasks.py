21721edb4b3e10577589fa3174a02631c5155c1e
rintao58511 <rintao1453@gmail.com>
2026-06-25 09:00:47 +0900

    feat: タスクの追加・一覧取得CRUDとルーターの繋ぎ込み完了

---
+++ app/routers/tasks.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+from schemas.tasks import UpdateAndCreateTaskSchema, TaskSchema, ResponseSchema
+from cruds.tasks import add_task, fetch_tasks
+
+router = APIRouter()
+
+
+@router.post("/tasks", response_model=ResponseSchema)
+async def create_task(task: UpdateAndCreateTaskSchema):
+    new_task = await add_task(task)
+    return ResponseSchema(message="タスク追加ができました", task=new_task)
+
+
+@router.get("/tasks", response_model=list[TaskSchema])
+async def list_task():
+    tasks = await fetch_tasks()
+    return tasks
