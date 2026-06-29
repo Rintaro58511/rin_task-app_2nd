16c9c30ad61ea3e028195a0186d786815906d3f6
github user name <rintao1453@gmail.com>
2026-06-29 19:44:12 +0900

    UserModelの変更

--- app/models/tasks.py
+++ app/models/tasks.py
@@ -3,25 +3,19 @@ from sqlalchemy import Text, Uuid, Date
 import uuid
 from datetime import date
 
+
 class Base(DeclarativeBase):
     pass
+
 
 class Task(Base):
     __tablename__ = "tasks"
     task_id: Mapped[uuid.UUID] = mapped_column(
-        Uuid,
-        primary_key=True,
-        default=uuid.uuid4
+        Uuid, primary_key=True, default=uuid.uuid4
     )
     task_name: Mapped[str] = mapped_column(
         Text,
-        nullable = False,
+        nullable=False,
     )
-    task_deadline: Mapped[date] = mapped_column(
-        Date,
-        nullable = False
-    )
-    task_detail: Mapped[str | None] = mapped_column(
-        Text,
-        nullable = True
-    )
+    task_deadline: Mapped[date] = mapped_column(Date, nullable=False)
+    task_detail: Mapped[str | None] = mapped_column(Text, nullable=True)
--- app/models/user.py
+++ app/models/user.py
@@ -2,33 +2,22 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship, DeclarativeBase
 from sqlalchemy import String, UUID, Boolean
 import uuid
 from typing import List
-#from models.tasks import Base, Task
+from models.tasks import Base, Task
 
-class Base(DeclarativeBase):
-    pass
 
 class User(Base):
     __tablename__ = "users"
     user_id: Mapped[uuid.UUID] = mapped_column(
-        UUID,
-        primary_key=True,
-        default=uuid.uuid4
+        UUID, primary_key=True, default=uuid.uuid4
     )
     user_name: Mapped[str] = mapped_column(
         String(50),
-        nullable = False,
+        nullable=False,
     )
     email: Mapped[str] = mapped_column(
         String(100),
-        nullable = False,
+        nullable=False,
     )
-    hashed_password: Mapped[str] = mapped_column(
-        String(255),
-        nullable=False
-    )
-    is_active: Mapped[bool] = mapped_column(
-        Boolean,
-        default=True,
-        nullable = False
-    )
-    #tasks: Mapped[List["Task"]] = relationship(back_populates="user")
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    tasks: Mapped[List["Task"]] = relationship(back_populates="user")
