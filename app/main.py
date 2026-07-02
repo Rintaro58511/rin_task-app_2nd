233c643b0bcb2af56aa27632f50ddb69563aa302
github user name <rintao1453@gmail.com>
2026-07-02 20:22:14 +0900

    middleware導入

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.tasks import router as task_router
 from routers.user import router as user_router
 
 app = FastAPI(
     title="タスク管理 & ユーザー認証 API",
     description="FastAPI + PostgreSQL で構築した、認証機能付きのタスク管理システムです。",
+)
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:5500",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(user_router, tags=["User (ユーザー認証)"])
