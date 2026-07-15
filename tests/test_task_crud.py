b881756e385c74c11a2d4040c362232ff3d19188
github user name <rintao1453@gmail.com>
2026-07-15 21:44:32 +0900

    test_fetch_tasksの実装

--- tests/test_task_crud.py
+++ tests/test_task_crud.py
@@ -5,7 +5,7 @@ import uuid
 from datetime import datetime, date
 
 from cruds import tasks
-from cruds.tasks import fetch_task, add_task
+from cruds.tasks import fetch_task, fetch_tasks, add_task
 from models.tasks import Task
 from schemas.tasks import TaskSchema, TaskStatus
 
@@ -46,8 +46,58 @@ async def test_fetch_task():
     mock_db.execute.assert_called_once()
 
 
-# async def fetch_task(task_id: UUID, db_session: AsyncSession) -> Task:
-#     result = await db_session.execute(select(Task).filter(Task.task_id == task_id))
-#     target_task = result.scalars().first()
+@pytest.mark.anyio
+async def test_fetch_tasks():
+    mock_db = AsyncMock()
 
-#     return target_task
+    user_id = uuid.uuid4()
+
+    expected_tasks = [
+        Task(
+            task_id=uuid.uuid4(),
+            task_name="test_task",
+            task_deadline=date(2026, 8, 1),
+            task_detail="コードのリファクタリング",
+            changed_time=datetime(2026, 7, 30, 11, 11, 11),
+            user=None,
+            user_id=user_id,
+            task_progress=TaskStatus.IN_PROGRESS,
+            progress_ratio=90,
+            progress_comment="終わりそう",
+        ),
+        Task(
+            task_id=uuid.uuid4(),
+            task_name="test_task2",
+            task_deadline=date(2026, 8, 2),
+            task_detail="コードのリファクタリング",
+            changed_time=datetime(2026, 7, 30, 11, 11, 12),
+            user=None,
+            user_id=user_id,
+            task_progress=TaskStatus.IN_PROGRESS,
+            progress_ratio=90,
+            progress_comment="終わりそう",
+        ),
+    ]
+
+    mock_results = MagicMock()
+    mock_db.execute.return_value = mock_results
+
+    mock_scalars = MagicMock()
+    mock_results.scalars.return_value = mock_scalars
+    mock_scalars.all.return_value = expected_tasks
+
+    retrieved_tasks = await fetch_tasks(mock_db, user_id)
+
+    assert retrieved_tasks[0].task_name == "test_task"
+    assert retrieved_tasks[1].task_name == "test_task2"
+    assert retrieved_tasks[0].task_deadline == date(2026, 8, 1)
+    assert retrieved_tasks[1].task_deadline == date(2026, 8, 2)
+    assert retrieved_tasks[0].changed_time == datetime(2026, 7, 30, 11, 11, 11)
+    assert retrieved_tasks[1].changed_time == datetime(2026, 7, 30, 11, 11, 12)
+
+
+# async def fetch_tasks(db_session: AsyncSession, user_id: UUID) -> list[Task]:
+#     results = await db_session.execute(select(Task).where(Task.user_id == user_id))
+#     target_tasks = results.scalars().all()
+
+#     return target_tasks
