eb994c7fb43f8a15f4c2d14f127a45a8aaf3b4ad
github user name <rintao1453@gmail.com>
2026-07-16 20:39:29 +0900

    remove_task, add_taskのテストコード実装

--- tests/test_task_crud.py
+++ tests/test_task_crud.py
@@ -5,9 +5,10 @@ import uuid
 from datetime import datetime, date
 
 from cruds import tasks
-from cruds.tasks import fetch_task, fetch_tasks, add_task
+from cruds.tasks import fetch_task, fetch_tasks, add_task, remove_task
 from models.tasks import Task
-from schemas.tasks import TaskSchema, TaskStatus
+from schemas.tasks import TaskSchema, TaskStatusSchema, UpdateAndCreateTaskSchema
+from enums import TaskStatus
 
 
 @pytest.mark.anyio
@@ -96,8 +97,70 @@ async def test_fetch_tasks():
     assert retrieved_tasks[1].changed_time == datetime(2026, 7, 30, 11, 11, 12)
 
 
-# async def fetch_tasks(db_session: AsyncSession, user_id: UUID) -> list[Task]:
-#     results = await db_session.execute(select(Task).where(Task.user_id == user_id))
-#     target_tasks = results.scalars().all()
+@pytest.mark.anyio
+async def test_add_task():
+    mock_db = AsyncMock()
 
-#     return target_tasks
+    user_id = uuid.uuid4()
+
+    status_data = TaskStatusSchema(
+        task_progress=TaskStatus.IN_PROGRESS,
+        progress_ratio=90,
+        progress_comment="終わりそう",
+    )
+
+    expected_task = UpdateAndCreateTaskSchema(
+        task_name="test_task2",
+        task_deadline=date(2026, 8, 2),
+        task_detail="コードのリファクタリング",
+        changed_time=datetime(2026, 7, 30, 11, 11, 12),
+        task_status=status_data,
+    )
+
+    returned_task = await add_task(expected_task, mock_db, user_id)
+
+    assert returned_task.task_name == "test_task2"
+    assert returned_task.user_id == user_id
+    assert returned_task.task_progress == TaskStatus.IN_PROGRESS
+    assert returned_task.progress_ratio == 90
+
+    mock_db.add.assert_called_once()
+    mock_db.commit.assert_called_once()
+    mock_db.refresh.assert_called_once()
+
+
+@pytest.mark.anyio
+async def test_remove_task(monkeypatch):
+    mock_db = AsyncMock()
+
+    task_id = uuid.uuid4()
+    user_id = uuid.uuid4()
+    expected_task = Task(
+        task_id=task_id,
+        task_name="test_task",
+        task_deadline=date(2026, 8, 1),
+        task_detail="コードのリファクタリング",
+        changed_time=datetime(2026, 7, 30, 11, 11, 11),
+        user=None,
+        user_id=user_id,
+        task_progress=TaskStatus.IN_PROGRESS,
+        progress_ratio=90,
+        progress_comment="終わりそう",
+    )
+
+    async def mock_fetch_task(mock_task_id, mock_db):
+        return expected_task
+
+    monkeypatch.setattr(tasks, "fetch_task", mock_fetch_task)
+
+    await remove_task(task_id, mock_db)
+
+    mock_db.delete.assert_called_once()
+    mock_db.commit.assert_called_once()
+
+
+# async def remove_task(task_id: UUID, db_session: AsyncSession) -> None:
+#     target_task = await fetch_task(task_id, db_session)
+#     if target_task:
+#         await db_session.delete(target_task)
+#         await db_session.commit()
