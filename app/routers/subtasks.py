7a4c47e4c2fda3aaf576e93006d6abb2b355045e
github user name <rintao1453@gmail.com>
2026-08-18 20:47:55 +0900

    サブタスクルーターにユーザー認証追加

--- app/routers/subtasks.py
+++ app/routers/subtasks.py
@@ -26,6 +26,7 @@ from cruds.subtasks import (
 from sqlalchemy.ext.asyncio import AsyncSession
 import db
 from uuid import UUID
+from routers.user import get_current_user
 from datetime import datetime, timezone
 
 router = APIRouter()
@@ -36,15 +37,16 @@ router = APIRouter()
 async def create_subtask(
     subtask: UpdateAndCreateSubTaskSchema,
     task_id: UUID,
+    current_user=Depends(get_current_user),
     db_session: AsyncSession = Depends(db.get_db_session),
 ) -> ResponseSchema:
     """サブタスクの追加を行い、エラーの場合はメッセージを返す"""
 
-    task = await fetch_task(task_id, db_session)
+    task = await fetch_task(task_id, current_user.user_id, db_session)
     if task is None:
         raise HTTPException(status_code=404, detail="タスクが存在しません")
     
-    await add_subtask(subtask, task_id, db_session)
+    await add_subtask(subtask, task_id, current_user.user_id, db_session)
     await db_session.flush()
 
     progress_ratio = await calculate_ratio(task_id, db_session)
@@ -52,6 +54,8 @@ async def create_subtask(
 
     task_progress = await check_progress(progress_ratio, db_session)
     task.task_progress = task_progress
+
+    task.changed_time = datetime.now(timezone.utc)
 
     await db_session.commit()
     return ResponseSchema(status_code=201, message="サブタスクを登録しました")
@@ -61,11 +65,12 @@ async def create_subtask(
 )
 async def search_subtask(
     subtask_id: UUID,
+    current_user=Depends(get_current_user),
     db_session: AsyncSession = Depends(db.get_db_session),
 ):
     """指定されたIDのサブタスクを取得する"""
 
-    subtask = await fetch_subtask(subtask_id, db_session)
+    subtask = await fetch_subtask(subtask_id, current_user.user_id, db_session)
     if subtask is None:
         raise HTTPException(status_code=404, detail="指定されたサブタスクが見つかりません")
 
@@ -76,11 +81,12 @@ async def search_subtask(
 )
 async def get_subtasks(
     task_id: UUID,
+    current_user=Depends(get_current_user),
     db_session: AsyncSession = Depends(db.get_db_session),
 ):
     """指定されたタスクIDのサブタスクを取得する"""
 
-    subtask_list = await fetch_subtasks(task_id, db_session)
+    subtask_list = await fetch_subtasks(task_id, current_user.user_id, db_session)
 
     return subtask_list
 
@@ -91,15 +97,16 @@ async def update_subtask(
     subtask_schema: UpdateAndCreateSubTaskSchema,
     subtask_id: UUID,
     task_id: UUID,
+    current_user=Depends(get_current_user),
     db_session: AsyncSession = Depends(db.get_db_session),
 ):
     """指定されたサブタスクIDのサブタスクを更新する"""
 
-    task = await fetch_task(task_id, db_session)
+    task = await fetch_task(task_id, current_user.user_id, db_session)
     if task is None:
         raise HTTPException(status_code=404, detail="指定されたタスクが存在しません")
 
-    target_subtask = await fetch_subtask(subtask_id, db_session)
+    target_subtask = await fetch_subtask(subtask_id, current_user.user_id, db_session)
 
     if target_subtask is None:
         raise HTTPException(status_code=404, detail="指定されたサブタスクが存在しません")
@@ -107,7 +114,7 @@ async def update_subtask(
     if task_id != target_subtask.task_id:
         raise HTTPException(status_code=400, detail="親タスクが異なります")
 
-    await modify_subtask(subtask_schema, target_subtask)
+    await modify_subtask(subtask_schema, current_user.user_id, target_subtask)
     await db_session.flush()
     
     progress_ratio = await calculate_ratio(task_id, db_session)
@@ -120,8 +127,6 @@ async def update_subtask(
 
     await db_session.commit()
 
-    print(f"progress_ratio: {progress_ratio}")
-
     return ResponseSchema(message="サブタスクを更新しました")
 
 @router.delete(
@@ -130,29 +135,32 @@ async def update_subtask(
 async def delete_subtask(
     subtask_id: UUID,
     task_id: UUID,
+    current_user=Depends(get_current_user),
     db_session: AsyncSession = Depends(db.get_db_session),
 ):
     """指定されたサブタスクIDのサブタスクを削除する"""
 
-    task = await fetch_task(task_id, db_session)
+    task = await fetch_task(task_id, current_user.user_id, db_session)
     if task is None:
         raise HTTPException(status_code=404, detail="指定されたタスクが存在しません")
 
-    target_subtask = await fetch_subtask(subtask_id, db_session)
+    target_subtask = await fetch_subtask(subtask_id, current_user.user_id, db_session)
     if target_subtask is None:
         raise HTTPException(status_code=404, detail="指定されたサブタスクが存在しません")
     if task_id != target_subtask.task_id:
         raise HTTPException(status_code=400, detail="親タスクが異なります")
 
-    await remove_subtask(target_subtask, db_session)
+    await remove_subtask(target_subtask, current_user.user_id, db_session)
     await db_session.flush()
         
     progress_ratio = await calculate_ratio(task_id, db_session)
     if progress_ratio is not None:
         task.progress_ratio = progress_ratio
+        
+        task_progress = await check_progress(progress_ratio, db_session)
+        task.task_progress = task_progress
 
-    task_progress = await check_progress(progress_ratio, db_session)
-    task.task_progress = task_progress
+    task.changed_time = datetime.now(timezone.utc)
 
     await db_session.commit()
 
