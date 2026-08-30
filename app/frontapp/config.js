526cb11f37844be7c59fdd3ce90a9b990b1c552a
github user name <rintao1453@gmail.com>
2026-08-31 03:41:52 +0900

    追加ファイルが登録されてなかった

---
+++ app/frontapp/config.js
@@ -0,0 +1,5 @@
+const API_BASE_URL =
+    window.location.hostname === "127.0.0.1" ||
+    window.location.hostname === "localhost"
+        ? "http://127.0.0.1:8002"
+        : "https://d25ee7cqp3i4lf.cloudfront.net";
